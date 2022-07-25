1360d1950aa6448283095cf8bfdc4aefc54f3a3e
Guy Jehu <guy.r.jehu@gmail.com>
2022-07-25 15:35:23 +0100

    modified:   data_processing/datprc.py Now reads list of subjects from subject_areas.conf and excludes overlaps to avoid doubling up once multiple lists involved

--- data_processing/datprc.py
+++ data_processing/datprc.py
@@ -1,7 +1,7 @@
+#import argparse
 import requests
 import xml.etree.ElementTree as ET
 import feedparser
-
 
 def parse_abstract(entry):
     #Returns abstract free of HTML and newline formatting.
@@ -33,21 +33,43 @@ def auth_string(auth_list):
         app = "".join(auth_list)
     return app
 
+def subject_overlaps(subject_areas,subject_info):
+    #Checks subject areas for overlaps to avoid doubling up entries
+    subs = subject_areas.copy()
+    subs.pop(0)
+    overlap = False
+    for sub in subs:
+        if sub in subject_info:
+            overlap = True
+            break
+    return overlap
+
+#Parsing the subject area
+with open('subject_areas.conf','r') as sfile:
+    lines=sfile.readlines()
+    subjects=[line.rstrip() for line in lines]
+#argp = argparse.ArgumentParser()
+#argp.add_argument('subj')
+#args = argp.parse_args()
+#print('Subject area:',subjects)
+
 #Getting the latest hep-th papers from the arxiv
 base_url = 'http://export.arxiv.org/rss/'
-subject = 'hep-th'
+subject = subjects[0]
 new_url = base_url+subject
 new_feed = feedparser.parse(new_url)
 
 entries = []
 #renders entries into format readable by frontend 
 for entry in new_feed.entries:
+    title_sp = entry.title.split(".")
+    if subject_overlaps(subjects,title_sp[-1]):
+        continue
+    entry_title = title_sp[0]
+#    print("Title: ",entry_title)
     link = entry.link
     arxiv_number = link.lstrip('http://arxiv.org/abs/')
 #    print("arXiv number: ", arxiv_number)
-    title_sp = entry.title.split(".")
-    entry_title = title_sp[0]
-#    print("Title: ",entry_title)
     abstract = parse_abstract(entry)
     author_list = parse_authors(entry)
     auths = auth_string(author_list)
