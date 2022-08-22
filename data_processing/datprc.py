a3ed65ecea05492ddbc46c51fb0462054cd21ae0
Guy Jehu <guy.r.jehu@gmail.com>
2022-08-22 14:27:47 +0200

    modified:   datprc.py updated python script to do multiple subjects. Improved checks for overlaps to include doubly cross-listed entries.

--- data_processing/datprc.py
+++ data_processing/datprc.py
@@ -1,18 +1,18 @@
-#import argparse
+import time
 import requests
 import xml.etree.ElementTree as ET
 import feedparser
 
+#Returns abstract free of HTML and newline formatting.
 def parse_abstract(entry):
-    #Returns abstract free of HTML and newline formatting.
     abstract = entry.description.lstrip("<p>")\
                                 .rstrip("</p>")\
                                 .rstrip()\
                                 .replace("\n"," ")
     return abstract
     
+#Returns entry authors as list
 def parse_authors(entry):
-    #Returns entry authors as list
     authors = entry.author
     authors = authors.split(",")
     authors = "<roo>" + "".join(authors) + "</roo>"
@@ -22,69 +22,73 @@ def parse_authors(entry):
         author_list.append(au.text)
     return author_list
 
+#Turns author list into a nice string.
 def auth_string(auth_list):
-    #Turns author list into a nice string.
     if len(auth_list)==1:
         app = auth_list[0]
     else:
-        for ii in range(len(auth_list)-2):
-            author_list[ii] = author_list[ii] + ", " 
-        author_list[-1] = " and "+author_list[-1]
-        app = "".join(auth_list)
+        app = ""
+        for ii in range(len(auth_list)-1):
+            app = app + auth_list[ii]+ ", "
+        app = app + "and "+auth_list[-1]
     return app
 
-def subject_overlaps(subject_areas,subject_info):
-    #Checks subject areas for overlaps to avoid doubling up entries
-    subs = subject_areas.copy()
-    subs.pop(0)
+#Checks subject areas for overlaps to avoid doubling up entries
+def subject_overlaps(subj,subject_areas,entry):
+    ent_sub = entry.title.split(".")[-1]
+    subjects = subject_areas.copy()
+    subjects.remove(subj)
     overlap = False
-    for sub in subs:
-        if sub in subject_info:
+    for subject in subjects:
+        if subject in ent_sub:
             overlap = True
             break
+    arxiv_number = entry.link.lstrip('http://arxiv.org/abs/')
+    if arxiv_number in watchlist:
+        overlap = True
+    if not subj in ent_sub:
+        watchlist.append(arxiv_number)
     return overlap
+
+#Queries arxiv for new entries given subject area and saves 
+#file in format designed to be parsed by C-written frontend
+def get_new_entries(subject,subjects):
+    base_url = 'http://export.arxiv.org/rss/'
+    new_url = base_url+subject
+    new_feed = feedparser.parse(new_url)
+    entries = []
+    #renders entries into format readable by frontend 
+    for entry in new_feed.entries:
+        if subject_overlaps(subject,subjects,entry):
+            continue
+        entry_title = entry.title.split(".")[0]
+        arxiv_number = entry.link.lstrip('http://arxiv.org/abs/')
+        abstract = parse_abstract(entry)
+        author_list = parse_authors(entry)
+        auths = auth_string(author_list)
+        entry = ("\n",arxiv_number,
+                  entry_title,
+                   auths,abstract)
+        entry_text = "\n".join(entry)
+        entries.append(entry_text)
+    content = "".join(entries)
+    with open(subject, 'w+') as file:
+        file.write(content)
 
 #Parsing the subject area
 with open('subjects.conf','r') as sfile:
     lines=sfile.readlines()
     subjects=[line.rstrip() for line in lines]
-#argp = argparse.ArgumentParser()
-#argp.add_argument('subj')
-#args = argp.parse_args()
-#print('Subject area:',subjects)
 
-#Getting the latest hep-th papers from the arxiv
-base_url = 'http://export.arxiv.org/rss/'
-subject = subjects[0]
-new_url = base_url+subject
-new_feed = feedparser.parse(new_url)
-#date = new_feed['date']
-#date = date.split(" ")
-#fname = ('_').join(date[1:4])+'_'+subject
-#print(fname)
+#watchlist is for fernickity cases of overlaps where the 
+#subject isn't covered but it could be in two crosslists
+watchlist = []
 
-entries = []
-#renders entries into format readable by frontend 
-for entry in new_feed.entries:
-    title_sp = entry.title.split(".")
-    if subject_overlaps(subjects,title_sp[-1]):
-        continue
-    entry_title = title_sp[0]
-#    print("Title: ",entry_title)
-    link = entry.link
-    arxiv_number = link.lstrip('http://arxiv.org/abs/')
-#    print("arXiv number: ", arxiv_number)
-    abstract = parse_abstract(entry)
-    author_list = parse_authors(entry)
-    auths = auth_string(author_list)
-#    print("Author: ",auths)
-    entry = ("\n",arxiv_number,
-              entry_title,
-               auths,abstract)
-    entry_text = "\n".join(entry)
-    entries.append(entry_text)
-content = "".join(entries)
-
-with open(subject, 'w+') as file:
-    file.write(content)
-
+#Loops through the subject areas, pausing to overcome
+#query limit from arXiv API
+get_new_entries(subjects[0],subjects)
+print("Entries for "+subjects[0] +" downloaded")
+for ii in range(len(subjects)-1):
+    time.sleep(3)
+    get_new_entries(subjects[ii+1],subjects)
+    print("Entries for "+subjects[ii+1] +" downloaded")
