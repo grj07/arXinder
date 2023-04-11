d14ba1c528ae5fc6716d4b80f6e8cd79f0e7f5e2
Guy Jehu <guy.r.jehu@gmail.com>
2023-04-11 15:23:49 +0100

    Ignoring to do list and new feeds

---
+++ state/saved_entries.d
@@ -0,0 +1,8 @@
+{"subject": "hep-th" ,"datef": "Tue 11 Apr 2023"}
+
+2304.04654
+Integrable Feynman Graphs and Yangian Symmetry on the Loom
+Vladimir Kazakov,Fedor Levkovich-Maslyuk,Victor Mishnyakov
+We extend the powerful property of Yangian invariance to a new large class of conformally invariant Feynman integrals. Our results apply to planar Feynman diagrams in any spacetime dimension dual to an arbitrary network of intersecting straight lines on a plane (Baxter lattice), with propagator powers determined by the geometry. We formulate Yangian symmetry in terms of a chain of Lax operators acting on the fixed coordinates around the graph, and we also extend this construction to the case of infinite-dimensional auxiliary space. Yangian invariance leads to new differential and integral equations for individual, highly nontrivial, Feynman graphs, and we present them explicitly for several examples. The graphs we consider determine correlators in the recently proposed loom fishnet CFTs. We also describe a generalization to the case with interaction vertices inside open faces of the diagram. Our construction unifies and greatly extends the known special cases of Yangian invariance to likely the most general family of integrable scalar planar graphs.
+{"subject": "hep-th" ,"datef": "Mon 10 Apr 2023"}
+{"subject": "hep-th" ,"datef": "Fri 07 Apr 2023"}
